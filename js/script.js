f10f3c2e7bbbc8146dda550c02fb88edf7141a91
Manuel Gil <manuelyeixon@gmail.com>
2016-05-12 17:09:43 -0400

    Cambio de apariencia, animaciones, entre otros

--- js/script.js
+++ js/script.js
@@ -1,33 +1,87 @@
 $(document).ready(function(){
-	$('#url').keyup(function(event){
-		if (event.keyCode == 13) {
-			$.ajax({
-				type: 'post',
-				url:  'php/getHtml.php',
-				data: { url:$('#url').val() },
-				beforeSend: function(){
-					$('.aimg').fadeOut();
-					$('.alert').addClass('hide');
-				  	$('#loader').removeClass('hide');
-				},
-				success: function(data){
-					if(data.indexOf('https') > -1){
-						$('.alert, #loader').addClass('hide');
-						$('.header').animate({ 'margin-top': '0px' }, 300, function() { 
-							$('#img').attr('src', data).parent().fadeIn('slow'); 
-						});
-					}else{
-						$('.aimg').fadeOut();
-						$('.alert').removeClass('hide');
-				  		$('#loader').addClass('hide');
-						$('.header').animate({ 'margin-top': '20%' }, 300, function() { });
-					}
-				}
-			});
-		}
-	});
+	$('#url').keyup(function(e){ if (e.keyCode == 13)  search(); });
+	$('.buscar').click(function(){ search(); });
 
-	$('#img').click(function(){
-		window.open('php/downloadPhoto.php?url_img=' + $('#img').attr('src'), '_blank');
+	$('.result img').click(function(){
+		window.open('php/downloadPhoto.php?url_img=' + $(this).prop('src'), '_blank');
 	});
 });
+
+function search () {
+	if (isValidUrl($('#url').val(), 1, 0)) {
+		$.ajax({
+			type: 'post',
+			url:  'php/getHtml.php',
+			dataType: 'json',
+			data: { url:$('#url').val() },
+			beforeSend: function(){
+				$('.result').hide();
+				$('.alert').addClass('hide');
+			  	$('.loading').removeClass('hide');
+			},
+			success: function(data){
+				if (data['photo'].indexOf('https') > -1) {
+					$('.alert, .loading').addClass('hide');
+					$('.header').animate({ 'margin-top': '63px' }, 300, function() { 
+						$('.result header').html(data['username']).next().prop('src', data['photo']).parent().show();
+						$('html, body').animate({ scrollTop: $('.result').offset().top }, 0);
+						animate('.result', 'zoomInUp');
+					});
+				} else 
+					showError();
+			},
+		   error: function() { showError(); }
+			,timeout: 10000
+		});
+	} else 
+		showError();
+}
+
+function showError() {
+	$('.result').hide();
+	$('.alert').removeClass('hide');
+	$('.loading').addClass('hide');
+	$('.header').animate({ 'margin-top': '20%' }, 300);
+	animate('#url, .buscar', 'shake')
+}
+
+function animate(el, x) {
+	$(el)
+		.removeClass(x + ' animated')
+		.addClass(x + ' animated')
+		.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+			function(){ $(el).removeClass(x + ' animated'); });
+}
+
+/**
+ * Tiene que recibir:
+ *  - la url a revisar
+ *  - indicar si es obligatorio [1|0]. Si es obligatorio, devuelve
+ *    false si la url esta vacia
+ *  - indicar si validamos que la direccion pueda ser de un servidor
+ *    ftp [1|0]
+ * Devuelve True o False
+ */
+function isValidUrl(url,obligatory,ftp) {
+    // Si no se especifica el paramatro "obligatory", interpretamos
+    // que no es obligatorio
+    if(obligatory==undefined)
+        obligatory=0;
+    // Si no se especifica el parametro "ftp", interpretamos que la
+    // direccion no puede ser una direccion a un servidor ftp
+    if(ftp==undefined)
+        ftp=0;
+ 
+    if(url=="" && obligatory==0)
+        return true;
+ 
+    if(ftp)
+        var pattern = /^(http|https|ftp)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi;
+    else
+        var pattern = /^(http|https)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi;
+ 
+    if(url.match(pattern))
+        return true;
+    else
+        return false;
+}
--- php/getHtml.php
+++ php/getHtml.php
@@ -3,9 +3,11 @@
 	$html = '';
 	$file = fopen($_POST['url'], 'r') or die ('No se encontró la página.');
 
-	while (!feof($file)) {
+	while (!feof($file))
 		$html .= fgets($file, 4096);
-	} fclose ($file);
 	
-	echo explode('"', substr(preg_replace('[\s+]', '',$html), 1508))[123];
+	fclose ($file);
+	
+	$data = explode('"', substr(preg_replace('[\s+]', '', $html), 1508));
+	echo json_encode(array('username' => explode('@', $data[109])[1], 'photo' => $data[123]));
 ?>
