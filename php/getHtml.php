fcb016caa198fc4d965f152fe6ff507bf0b1bf17
Manuel Gil <mauelyeixon@gmail.com>
2016-11-06 17:21:26 -0400

    fix bug searching photo...

--- php/getHtml.php
+++ php/getHtml.php
@@ -2,6 +2,5 @@
 	$content = file_get_contents($_POST['url']) or die('Not Found');
 
 	$data = explode('"', str_replace(array(" ", "\n", "\r"), "", $content));
-	
-	echo json_encode(array('username' => explode('@', $data[143])[1], 'photo' => $data[157]));
+	echo json_encode(array('username' => explode('@', $data[161])[1], 'photo' => $data[175]));
 ?>
